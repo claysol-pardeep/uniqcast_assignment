25f52de532c0a6becf8192eeab4337fb06d1b5b0
Pardeep Kumar <pardeepsingal1@gmail.com>
2020-12-01 17:10:33 +0530

    Uniqcast assignment

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# uniqcast_assignment
-Assignment
+# UniqCast Assignment
+
+Task to accept the local file path and return the docker mount volume video url.
+
+## Requirements
+
+-   we use [node.js](https://nodejs.org/en/) `lts/carbon`.
+
+    if you have [`nvm`](https://github.com/creationix/nvm) installed it should pick up the version from the `.nvmrc` file and do the rest for you.
+
+-   we use [yarn](https://yarnpkg.com/en/docs/install)
+
+---
+
+## 🚀 Getting Started
+
+#### 1. Clone and Install
+
+```bash
+# Clone the repo
+git clone https://github.com/claysol-pardeep/uniqcast_assignment
+
+# Install node dependencies
+yarn
+```
+
+#### 2. Run the App Local
+
+```bash
+# Start the App
+npm run dev
+```
+
+---
+
+## 📚 Libraries
+
+### Node Libraries
+
+-   [nodejs](https://nodejs.org/en/download/)
+-   [NATS](https://www.npmjs.com/package/nats)
+-   [experss-js](https://expressjs.com/)
+
+#### Docker Commands
+
+docker run -d -p 80:3000 -v ~/assets:/usr/src/app/assets --name node-app uniqcast
+docker volume create assets
+docker volume ls
+docker volume inspect assets
+docker-compose down
+docker-compose up
+
+NATS Port : 0.0.0.0:8222 / 4222
---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const router = express.Router();
+const fs = require('fs');
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+const NATS = require('nats');
+const nc = NATS.connect('nats://localhost:8222');
+
+router.post('/video', (req, res) => {
+	let source = req.body.file;
+	let target = '/Users/apple/assets/' + Date.now() + '.mp4';
+	fs.copyFile(source, target, (err) => {
+		if (err) throw err;
+
+		fs.stat(target, (err, stat) => {
+			// Handle file not found
+			if (err !== null && err.code === 'ENOENT') {
+				res.sendStatus(404);
+			}
+			const fileSize = stat.size;
+			const range = req.headers.range;
+			if (range) {
+				const parts = range.replace(/bytes=/, '').split('-');
+
+				const start = parseInt(parts[0], 10);
+				const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+
+				const chunksize = end - start + 1;
+				const file = fs.createReadStream(target, { start, end });
+				const head = {
+					'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+					'Accept-Ranges': 'bytes',
+					'Content-Length': chunksize,
+					'Content-Type': 'video/mp4',
+				};
+				nc.publish('video-channel', chunksize);
+				res.writeHead(206, head);
+				file.pipe(res);
+			} else {
+				const head = {
+					'Content-Length': fileSize,
+					'Content-Type': 'video/mp4',
+				};
+				nc.publish('video-channel', target);
+				res.writeHead(200, head);
+				fs.createReadStream(target).pipe(res);
+				///nc.unsubscribe(sid);
+			}
+		});
+	});
+});
+app.use(router);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+	console.log(`Server listening on port ${PORT}`);
+});
