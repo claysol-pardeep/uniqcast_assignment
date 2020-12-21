f243d6641a6aacb6ddd8979688d86076c925a151
Pardeep Kumar <pardeep.kumar@claysol.com>
2020-12-21 21:24:41 +0530

    Mp4 Parser solution

--- app.js
+++ app.js
@@ -1,111 +1,78 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const router = express.Router();
-const debug = require('debug')('uniquecast:server');
-const path = require('path');
-const multer = require('multer');
-const serveIndex = require('serve-index');
 const fs = require('fs');
+const readChunk = require('read-chunk');
+import MP4Parser from './mp4parser';
 const app = express();
-const NATS = require('nats');
-const servers = ['nats://nats:4222', 'nats://nats:8222', 'nats://nats:6222'];
-var ffmpeg = require('fluent-ffmpeg');
-const nc = NATS.connect({ servers: servers });
-//const nc = NATS.connect();
-nc.on('connect', () => {
-	console.log('Connected to ' + nc.currentServer.url.host);
-});
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-
-var storage = multer.diskStorage({
-	destination: (req, file, cb) => {
-		cb(null, './public/');
-	},
-	filename: (req, file, cb) => {
-		cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-	},
-});
-
-const upload = multer({ storage: storage });
-
 //get the router
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/ftp', express.static('frontend'), serveIndex('public', { icons: true }));
-app.post('/segment', function (req, res) {
-	return ffmpeg.ffprobe('uploads/1.mp4', function (err, metadata) {
-		return res.send(metadata);
-	});
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+/* NATS Configuration */
+const NATS = require('nats');
+const servers = ['nats://localhost:4222', 'nats://localhost:8222', 'nats://localhost:6222'];
+const nc = NATS.connect({ servers: servers });
+nc.on('connect', () => {
+	console.log('Connected to ' + nc.currentServer.url.host);
 });
+
+/* Message handling */
+nc.subscribe('video', function (file) {
+	let parser = new MP4Parser(fs.createReadStream(file));
+	parser.on('atom', (atom) => {
+		var seq = '0' + atom._seq;
+		seq = seq.substring(seq.length - 2, seq.length);
+		if (atom.type === 'moov') {
+			return readChunk(file, 0, atom._pos).then((fileData) => {
+				let filePath = 'uploads/convert-' + new Date().getTime() + '.mp4';
+				fs.writeFile(filePath, fileData, function (err) {
+					if (err) return console.log(err);
+					return res.send({ filePath });
+				});
+			});
+		}
+	});
+	parser.on('data_mdat', (chunk) => {
+		//console.log(chunk.length);
+	});
+	parser.start();
+});
+
+/* Testing the file contents */
 app.get('/', function (req, res) {
-	nc.publish('video', 'file data processing.');
-	return res.send('Welcome in UniqueCast Assignment!');
-});
-nc.subscribe('record', function (msg) {
-	console.log('Received a message: ' + msg);
-});
-// NATS Subscribe the Channel for testing the receive message.
-nc.subscribe('video', function (msg) {
-	console.log('Received a message: ' + msg);
-	ffmpeg.ffprobe('uploads/1.mp4', function (err, metadata) {
-		nc.publish('record', JSON.stringify(metadata));
+	const file = 'uploads/1.mp4';
+	let parser = new MP4Parser(fs.createReadStream(file));
+	parser.on('atom', (atom) => {
+		var seq = '0' + atom._seq;
+		seq = seq.substring(seq.length - 2, seq.length);
+		if (atom.type === 'moov') {
+			return readChunk(file, 0, atom._pos).then((fileData) => {
+				let filePath = 'uploads/convert-' + new Date().getTime() + '.mp4';
+				fs.writeFile(
+					filePath,
+					fileData,
+					{
+						encoding: 'base64',
+					},
+					function (err) {
+						if (err) return console.log(err);
+						return res.send({ filePath });
+					}
+				);
+			});
+		}
+		console.log(
+			`${seq}. |${new Array(atom._level * 3).join('-')}${atom.type}(size:${atom.size}, pos:${atom._pos})`
+		);
 	});
-});
-
-/* Micro-service that upload the file in docker mounted directory and return the file information */
-app.post('/upload', upload.single('file'), function (req, res) {
-	let url = req.hostname + ':3000/ftp/assets/' + req.file.filename;
-	nc.publish('video-channel', url);
-	return res.send(req.file);
-});
-
-/**** Outdated MP4 Processing with FFMPEG file processing ****/
-router.post('/video', (req, res) => {
-	let source = req.body.file;
-	console.log(`source url is : ${source}`);
-	let target = '/Users/apple/assets/' + Date.now() + '.mp4';
-	fs.copyFile(source, target, (err) => {
-		if (err) throw err;
-
-		fs.stat(target, (err, stat) => {
-			// Handle file not found
-			if (err !== null && err.code === 'ENOENT') {
-				res.sendStatus(404);
-			}
-			const fileSize = stat.size;
-			const range = req.headers.range;
-			if (range) {
-				const parts = range.replace(/bytes=/, '').split('-');
-
-				const start = parseInt(parts[0], 10);
-				const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
-
-				const chunksize = end - start + 1;
-				const file = fs.createReadStream(target, { start, end });
-				const head = {
-					'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-					'Accept-Ranges': 'bytes',
-					'Content-Length': chunksize,
-					'Content-Type': 'video/mp4',
-				};
-				// nc.publish('video-channel', chunksize);
-				res.writeHead(206, head);
-				file.pipe(res);
-			} else {
-				const head = {
-					'Content-Length': fileSize,
-					'Content-Type': 'video/mp4',
-				};
-				nc.publish('channel', target);
-				res.writeHead(200, head);
-				fs.createReadStream(target).pipe(res);
-				///nc.unsubscribe(sid);
-			}
-		});
+	parser.on('data_mdat', (chunk) => {
+		console.log(chunk.length);
 	});
+	parser.start();
 });
 
 app.use(router);
---
+++ atom.js
@@ -0,0 +1,19 @@
+export default class Atom {
+	constructor(size, type, pos, seq) {
+		this.size = size;
+		this.type = type;
+		this._seq = 0;
+		this._level = 0;
+		this._pos = pos;
+		this._children = [];
+		this._seq = seq;
+	}
+	includes(atom) {
+		return this._pos < atom._pos && this._pos + this.size >= atom._pos + atom.size;
+	}
+	setParent(atom) {
+		this._level = atom._level + 1;
+		this._parent = atom;
+		atom._children.push(this);
+	}
+}
